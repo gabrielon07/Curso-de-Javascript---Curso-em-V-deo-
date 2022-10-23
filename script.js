74582313dc8476e9fa0ef090383d48f8bdbdc205
Gabriel <gabriel.codeman@gmail.com>
2022-10-23 20:23:36 -0300

    complete exercise

--- script.js
+++ script.js
@@ -5,5 +5,45 @@ function verificar() {
     var res = document.getElementById("res");
     if (fano.value.length == 0 || fano.value > ano) {
       window.alert("[ERRO] Verifique os dados e tente novamente!");
-    } 
+    } else {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      var genêro = ''
+      var img = document.createElement('img')
+      img.setAttribute('id', 'foto')
+      if (fsex[0].checked){
+        genêro = 'Homem'
+        if (idade >= 0 && idade < 10){
+          //criança
+          img.setAttribute('src', 'foto-bebe-m.png')
+        }else if (idade < 21){
+          //jovem
+          img.setAttribute('src', 'foto-jovem-m.png')
+        } else if (idade < 50){
+          //adulto
+          img.setAttribute('src', 'foto-adulto-m.png')
+        }else{
+          //idoso
+          img.setAttribute('src', 'foto-idoso-m.png')
+        }
+      } else if (fsex[1].checked) {
+        genêro = 'Mulher'
+        if (idade >= 0 && idade < 10){
+          //criança
+          img.setAttribute('src', 'foto-bebe-f.png')
+        }else if (idade < 21){
+          //jovem
+          img.setAttribute('src', 'foto-jovem-f.png')
+        } else if (idade < 50){
+          //adulto 
+          img.setAttribute('src', 'foto-adulto-f.png')
+        }else {
+          //idoso
+          img.setAttribute('src', 'foto-idoso-f.png')
+        }
+      }
+      res.style.textAlign = 'center'
+      res.innerHTML = `Detectamos ${genêro} com ${idade} anos.`
+      res.appendChild(img)
+    }
 }
